74c3747cbcb6fcf5647f58fc7cd84ec35554476e
Shre-D <shreyash1264@gmail.com>
2024-05-06 19:24:30 +0530

    feat: complete home and about us

--- src/components/gallery-main/gallery.tsx
+++ src/components/gallery-main/gallery.tsx
@@ -10,13 +10,11 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 
-const pictures = [
-    "https://scontent.fbom19-1.fna.fbcdn.net/v/t39.30808-6/307559152_456647143159292_3134832321580569711_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kGmwBbamGHgQ7kNvgG1yf92&_nc_ht=scontent.fbom19-1.fna&oh=00_AfBQKs8JebtOPsnb8wu5GrIWzq3E8oHCs2kzmLfN0PNtIA&oe=66388BC9",
-    "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
-    "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8"
-]
+interface Props {
+    pictures: string[]
+}
 
-export default function MainGallery() {
+export default function MainGallery({pictures} : Props) {
     return (
         <Carousel
             className="relative lg:px-[50vh] px-2 md:px-[20vh] h-[80vh] -z-10 -mb-10"
