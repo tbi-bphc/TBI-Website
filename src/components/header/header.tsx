3f84ccae916ad624d0f613b5d194b243ba7adb37
Shre-D <shreyash1264@gmail.com>
2024-05-27 14:38:39 +0530

    feat: update contacts, event page and incubatees

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,15 @@
 function Header() {
     return (
-        <header className="bg-gray-800 py-4 lg:px-[50vh] md:px-[10vh] px-2">
+        <header className=" bg-nord4 py-4 lg:px-[55vh] md:px-[10vh] px-2">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center">
-                    <p className="h-8 w-auto mr-4 text-white"> TBI  </p>
+                    {/* <p className="h-8 w-auto mr-4 text-white"> TBI  </p> */}
+                    <img src="/tbislogoblue 3.png" alt="" className="w-40 h-24" />
                 </div>
                 <div className="flex items-center text-white">
-                    <p className="h-8 w-auto mr-4"> BITS </p>
+                    {/* <p className="h-8 w-auto mr-4"> BITS </p> */}
+                    <img src="/BITS_Pilani-Logo 4.png" alt="" className="w-24 h-24" />
+
                 </div>
             </div>
         </header>
