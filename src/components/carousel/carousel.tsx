86d709b049d5d2e045fd3b22694223d51e000f92
Shre-D <shreyash1264@gmail.com>
2024-05-27 15:50:06 +0530

    feat: update gallery and images for those

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -10,12 +10,19 @@ import {
 } from "@/components/ui/carousel"
 
 const pictures = [
-  "https://scontent.fbom19-1.fna.fbcdn.net/v/t39.30808-6/307559152_456647143159292_3134832321580569711_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kGmwBbamGHgQ7kNvgG1yf92&_nc_ht=scontent.fbom19-1.fna&oh=00_AfBQKs8JebtOPsnb8wu5GrIWzq3E8oHCs2kzmLfN0PNtIA&oe=66388BC9",
-  "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
-  "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
-  "https://scontent.fbom19-1.fna.fbcdn.net/v/t39.30808-6/307559152_456647143159292_3134832321580569711_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kGmwBbamGHgQ7kNvgG1yf92&_nc_ht=scontent.fbom19-1.fna&oh=00_AfBQKs8JebtOPsnb8wu5GrIWzq3E8oHCs2kzmLfN0PNtIA&oe=66388BC9",
-  "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
-  "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
+  "/startups/abyom.png",
+  "/startups/alfaleus.png",
+  "/startups/anjamma.png",
+  "/startups/Applied_Aero_Labs.png",
+  "/startups/atreves.png",
+  "/startups/cinnova.png",
+  "/startups/cleome.png",
+  "/startups/loka.png",
+  "/startups/masuu.png",
+  "/startups/pharmajen.png",
+  "/startups/pyrome.png",
+  "/startups/sensome.png",
+  "/startups/tangerne.png",
 ]
 
 export function MultiImageCarousel() {
--- src/pages/about/aboutgallery.tsx
+++ src/pages/about/aboutgallery.tsx
@@ -1,8 +1,9 @@
 import MainGallery from "@/components/gallery-main/gallery";
 const pictures = [
-    "https://scontent.fbom19-1.fna.fbcdn.net/v/t39.30808-6/307559152_456647143159292_3134832321580569711_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kGmwBbamGHgQ7kNvgG1yf92&_nc_ht=scontent.fbom19-1.fna&oh=00_AfBQKs8JebtOPsnb8wu5GrIWzq3E8oHCs2kzmLfN0PNtIA&oe=66388BC9",
-    "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8",
-    "https://qph.cf2.quoracdn.net/main-qimg-0052d1208cffaee2dce84b0ec33ae1a8"
+    "/gallery/audi.png",
+    "/gallery/lawns.png",
+    "/gallery/nabgate.png",
+    "/gallery/classroom-1.jpg"
 ]
 
 export default function AboutGallery() {
