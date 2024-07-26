35889f0235e281b74e462fc532ea98315b36de7a
Shre-D <shreyash1264@gmail.com>
2024-07-26 05:34:49 +0530

    feat: programmes initialised

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { Menu, X } from 'lucide-react';
-import { ChevronDown } from 'lucide-react';
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,30 +16,29 @@ function Navbar() {
               onClick={() => setIsOpen(!isOpen)}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             >
-              {isOpen ? (
-                <X size={24} />
-              ) : (
-                <Menu size={24} />
-              )}
+              {isOpen ? <X size={24} /> : <Menu size={24} />}
             </button>
           </div>
           {/* Navigation Links - Desktop */}
           <div className="hidden md:flex justify-center items-center space-x-4">
-            <NavLink title="Home" link="/home"/>
-            <NavLink title="About Us" link='/about' />
-            <NavLink title="Incubatees" link="/incubatees"/>
-            <NavLink title="Events" link='/events'/>
-            <NavLink title="Team" link='/team'/>
-            <NavLink title="Facilities" link='/facilities'/>
+            <NavLink title="Home" link="/home" />
+            <NavLink title="About Us" link="/about" />
+            <NavLink title="Incubatees" link="/incubatees" />
+            <NavLink title="Events" link="/events" />
+            <NavLink title="Team" link="/team" />
+            <NavLink title="Facilities" link="/facilities" />
             {/* Programmes Dropdown - Desktop */}
             <div className="relative inline-block text-left">
               <button
                 onClick={() => setIsProgrammesOpen(!isProgrammesOpen)}
                 type="button"
-                className="inline-flex justify-center w-full rounded-md border text-white border-gray-300  px-4 py-2 text-sm font-medium hover:bg-gray-700 focus:outline-none  focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                className="inline-flex justify-center w-full rounded-md text-white  px-4 py-2 text-sm font-medium hover:bg-gray-700 focus:outline-none  focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
               >
                 Programmes
-                <ChevronDown className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                <ChevronDown
+                  className="-mr-1 ml-2 h-5 w-5"
+                  aria-hidden="true"
+                />
               </button>
               {isProgrammesOpen && (
                 <div
@@ -50,21 +49,21 @@ function Navbar() {
                 >
                   <div className="py-1" role="none">
                     <a
-                      href="#"
+                      href="/programmes/incubation"
                       className="block px-4 py-2 text-sm hover:bg-gray-700 hover:text-gray-50"
                       role="menuitem"
                     >
                       Incubation
                     </a>
                     <a
-                      href="#"
+                      href="programmes/seep"
                       className="block px-4 py-2 text-sm hover:bg-gray-700 hover:text-gray-50"
                       role="menuitem"
                     >
                       SEEP
                     </a>
                     <a
-                      href="#"
+                      href="/programmes/solve"
                       className="block px-4 py-2 text-sm hover:bg-gray-700 hover:text-gray-50"
                       role="menuitem"
                     >
@@ -75,7 +74,7 @@ function Navbar() {
               )}
             </div>
             {/* <NavLink title="Partners" /> */}
-            <NavLink title="Contact Us" link="/contact"/>
+            <NavLink title="Contact Us" link="/contact" />
           </div>
         </div>
       </div>
@@ -83,10 +82,10 @@ function Navbar() {
       {isOpen && (
         <div className="md:hidden bg-gray-800 py-2">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <NavLink title="Home" link="/home"/>
-            <NavLink title="About Us" link="/about"/>
-            <NavLink title="Incubatees" link="/incubatees"/>
-            <NavLink title="Events Gallery" link="/events"/>
+            <NavLink title="Home" link="/home" />
+            <NavLink title="About Us" link="/about" />
+            <NavLink title="Incubatees" link="/incubatees" />
+            <NavLink title="Events Gallery" link="/events" />
             {/* Programmes Dropdown - Mobile */}
             <div className="relative inline-block text-left">
               <button
@@ -95,7 +94,10 @@ function Navbar() {
                 className="inline-flex justify-center w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
               >
                 Programmes
-                <ChevronDown className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                <ChevronDown
+                  className="-mr-1 ml-2 h-5 w-5"
+                  aria-hidden="true"
+                />
               </button>
               {isProgrammesOpen && (
                 <div
@@ -106,21 +108,21 @@ function Navbar() {
                 >
                   <div className="py-1" role="none">
                     <a
-                      href="#"
+                      href="/programmes/incubation/"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                       role="menuitem"
                     >
                       Incubation
                     </a>
                     <a
-                      href="#"
+                      href="/programmes/seep/"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                       role="menuitem"
                     >
                       SEEP
                     </a>
                     <a
-                      href="#"
+                      href="/programmes/solve/"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                       role="menuitem"
                     >
@@ -131,7 +133,7 @@ function Navbar() {
               )}
             </div>
             {/* <NavLink title="Partners" /> */}
-            <NavLink title="Contact Us" link="/contact"/>
+            <NavLink title="Contact Us" link="/contact" />
           </div>
         </div>
       )}
@@ -141,7 +143,7 @@ function Navbar() {
 
 interface NavLinkProps {
   title: string;
-  link:string;
+  link: string;
 }
 
 function NavLink({ title, link }: NavLinkProps) {
