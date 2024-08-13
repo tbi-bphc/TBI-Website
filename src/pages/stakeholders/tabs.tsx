c72bcc5cda87db755460e908942acee3be308f0b
Shre-D <shreyash1264@gmail.com>
2024-08-13 11:39:33 +0530

    feat: restructure stakeholders page

--- src/pages/stakeholders/tabs.tsx
+++ src/pages/stakeholders/tabs.tsx
@@ -4,61 +4,89 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
-import Mentorcard from "@/components/cards/mentorcard.astro"
+
+interface Props {
+    name: string,
+    image?: string,
+    description?: string,
+}
+function Element({ name, image, description }: Props) {
+    return (
+        <>
+            <div
+                className="w-full my-4 md:max-w-72 items-center bg-white shadow-lg rounded-lg flex flex-col justify-center place-self-center"
+            >
+                <img
+                    src={image ?? "https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp"}
+                    alt="img"
+                    title="img"
+                    className="w-full h-auto object-cover rounded-t-lg"
+                />
+                <div className="w-full p-4 justify-start flex flex-col">
+                    <h4 className=" text-xl text-center pb-1">
+                        {name}
+                    </h4>
+                    {
+                        description ?
+                            <p className="my-4 text-center">
+                                {description}
+                            </p> : <></>
+                    }
+
+                </div>
+            </div>
+        </>
+    )
+}
 
 function Stakeholders() {
 
-    const governingBody: any = ["Body-1", "Body-2", "Body-3", "Body-4"]
-    const entrepreneurshipCommittee: any = ["Committee-1", "Committee-2", "Committee-3", "Committee-4"]
-    const mentors: any = ["Mentor-1", "Mentor-2"]
-    const partnersSponsors: any = ["Partner-1"]
-    // const all = governingBody+entrepreneurshipCommittee+mentors+partnersSponsors;
+    const governingBody = ["Body-1", "Body-2", "Body-3", "Body-4"]
+    const entrepreneurshipCommittee = ["Committee-1", "Committee-2", "Committee-3", "Committee-4"]
+    const mentors = ["Mentor-1", "Mentor-2"]
+    const partnersSponsors = ["Partner-1"]
+    let all = [];
+    all.push(...governingBody)
+    all.push(...entrepreneurshipCommittee)
+    all.push(...mentors)
+    all.push(...partnersSponsors)
 
     return (
         <div>
-            <Tabs defaultValue="governing body" className=" px-[25vh] w-full">
-                <TabsList className="grid w-full grid-cols-4 mt-14 mx-auto my-8">
-                    {/* <TabsTrigger value="all">All</TabsTrigger> */}
-                    <TabsTrigger value="governing body">Governing Body</TabsTrigger>
-                    <TabsTrigger value="entrepreneurship committee">Entrepreneurship Committee</TabsTrigger>
-                    <TabsTrigger value="mentors">Mentors</TabsTrigger>
-                    <TabsTrigger value="partners and sponsors">Partners and Sponsors</TabsTrigger>
+            <Tabs defaultValue="all" className="px-2 md:px-6 lg:px-[25vh]">
+                <TabsList className="flex items-center justify-around flex-wrap h-auto space-y-1 mt-14 mx-auto my-8">
+                    <TabsTrigger className="flex-1" value="all">All</TabsTrigger>
+                    <TabsTrigger className="flex-1" value="governing body">Governing Body</TabsTrigger>
+                    <TabsTrigger className="flex-1" value="entrepreneurship committee">Entrepreneurship Committee</TabsTrigger>
+                    <TabsTrigger className="flex-1" value="mentors">Mentors</TabsTrigger>
+                    <TabsTrigger className="flex-1" value="partners and sponsors">Partners and Sponsors</TabsTrigger>
                 </TabsList>
-                {/* <TabsContent value="All">
+                <TabsContent value="all">
                     <div className="flex items-center justify-center">
-                        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
                             {
-                                <>
-                                    All
-                                </>
+                                all.map((body: any) =>
+
+                                    <div>
+                                        <Element name={body} />
+                                    </div>
+
+                                )
+
                             }
                         </section>
                     </div>
-                </TabsContent> */}
+                </TabsContent>
                 <TabsContent value="governing body">
                     <div className="flex items-center justify-center">
                         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-2">
                             {
                                 governingBody.map((body: any) =>
-                                    <>
-                                        <main
-                                            className="w-full my-4 md:max-w-72 items-center bg-white shadow-lg rounded-lg flex justify-center place-self-center"
-                                        >
-                                            <div>
-                                                <img
-                                                    src={
-                                                        "https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp"}
-                                                    alt="image"
-                                                    className="min-h-40"
-                                                />
-                                            </div>
-                                            <div className="my-auto mx-6">
-                                                <p className="text-left">
-                                                    {body}
-                                                </p>
-                                            </div>
-                                        </main>
-                                    </>
+
+                                    <div>
+                                        <Element name={body} />
+                                    </div>
+
                                 )
 
                             }
@@ -70,25 +98,11 @@ function Stakeholders() {
                         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-2">
                             {
                                 entrepreneurshipCommittee.map((body: any) =>
-                                    <>
-                                        <main
-                                            className="w-full my-4 md:max-w-72 items-center bg-white shadow-lg rounded-lg flex justify-center place-self-center"
-                                        >
-                                            <div>
-                                                <img
-                                                    src={
-                                                        "https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp"}
-                                                    alt="image"
-                                                    className="min-h-40"
-                                                />
-                                            </div>
-                                            <div className="my-auto mx-6">
-                                                <p className="text-left">
-                                                    {body}
-                                                </p>
-                                            </div>
-                                        </main>
-                                    </>
+
+                                    <div>
+                                        <Element name={body} />
+                                    </div>
+
                                 )
 
                             }
@@ -100,25 +114,11 @@ function Stakeholders() {
                         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-2">
                             {
                                 mentors.map((body: any) =>
-                                    <>
-                                        <main
-                                            className="w-full my-4 md:max-w-72 items-center bg-white shadow-lg rounded-lg flex justify-center place-self-center"
-                                        >
-                                            <div>
-                                                <img
-                                                    src={
-                                                        "https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp"}
-                                                    alt="image"
-                                                    className="min-h-40"
-                                                />
-                                            </div>
-                                            <div className="my-auto mx-6">
-                                                <p className="text-left">
-                                                    {body}
-                                                </p>
-                                            </div>
-                                        </main>
-                                    </>
+
+                                    <div>
+                                        <Element name={body} />
+                                    </div>
+
                                 )
 
                             }
@@ -129,28 +129,13 @@ function Stakeholders() {
                     <div className="flex items-center justify-center">
                         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 gap-y-2">
                             {
-                                governingBody.map((body: any) =>
-                                    <>
-                                        <main
-                                            className="w-full my-4 md:max-w-72 items-center bg-white shadow-lg rounded-lg flex justify-center place-self-center"
-                                        >
-                                            <div>
-                                                <img
-                                                    src={
-                                                        "https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp"}
-                                                    alt="image"
-                                                    className="min-h-40"
-                                                />
-                                            </div>
-                                            <div className="my-auto mx-6">
-                                                <p className="text-left">
-                                                    {body}
-                                                </p>
-                                            </div>
-                                        </main>
-                                    </>
-                                )
+                                partnersSponsors.map((body: any) =>
 
+                                    <div>
+                                        <Element name={body} />
+                                    </div>
+
+                                )
                             }
                         </section>
                     </div>
